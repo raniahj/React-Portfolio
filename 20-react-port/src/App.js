fe925c6141c666d8f267ce9ae77a87493e15f2da
raniahj <raniahj06@gmail.com>
2022-03-18 03:42:09 -0400

    submit About section

---
+++ 20-react-port/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import About from './components/About';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <main>
+        <About></About>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ 20-react-port/src/components/About/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function About() {
+  return (
+    <section className="About" id="about">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-7">
+            <h1> Raniah Jeanlys </h1>
+            <p> Recent Finance graduate and Fintech student.
+                 I have an understanding of Python, SQL, AWS, Machine Learning, Blockchain, and Solidity.
+                 Now in addition to that I understand Web Development concepts such as React,Javascript, HTML, and CSS.
+                  Also, proficiency in suite of applications such as Microsoft Word, Excel, PowerPoint, and Visio.  
+                  Outside of the classroom, I have a lot of experience in leadership positions and team-building environments.</p>
+                  </div>
+          <div class="col-md-5">
+            <div className="Headshot"></div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default About;
